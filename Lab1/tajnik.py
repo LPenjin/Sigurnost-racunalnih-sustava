f7609e644919ae334e8b4baca8bee5b12e9dd6dd
LPenjin <l.penjo@gmail.com>
2022-03-22 00:52:31 +0100

    Initial commit

---
+++ Lab1/tajnik.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='SRS labos',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+def main():
+    args = parse_arguments()
+
+
+if __name__ == '__main__':
+    main()
